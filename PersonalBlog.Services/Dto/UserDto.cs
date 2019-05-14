414b72c46aa35acd5088cdf91b462e76af820d13
Roman Myshchyshyn <roman1999.myshchyshyn@gmail.com>
2019-05-14 09:38:24 +0300

    authentication

--- PersonalBlog.Services/Dto/UserDto.cs
+++ PersonalBlog.Services/Dto/UserDto.cs
@@ -1,13 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PersonalBlog.Services.Dto
 {
     public class UserDto
     {
+        [JsonProperty("id")]
         public string Id { get; set; }
+
+        [Required]
+        [JsonProperty("name")]
         public string Name { get; set; }
+
+        [Required]
+        [JsonProperty("password")]
         public string Password { get; set; }
         public string PasswordHash { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [JsonProperty("email")]
         public string Email { get; set; }
 
         public List<string> RoleNames { get; set; }
